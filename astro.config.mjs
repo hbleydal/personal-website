1a0405c215ed4128d5c8777e537890b7b46da222
hbleydal <bley.helene@hotmail.fr>
2026-02-12 15:49:46 +0100

    add github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hbleydal.github.io',
+  base: '/personal-website/',
+  output: 'static',
+
+});
