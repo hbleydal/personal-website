958ae828643d48a05b56a6e5100ad26771cde593
hbleydal <bley.helene@hotmail.fr>
2026-02-12 17:23:15 +0100

    Fix header and url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hbleydal.github.io/personal-website',
+  site: 'https://hbleydal.github.io/personal-website/fr',
   base: '/personal-website',
   output: 'static',
 });
