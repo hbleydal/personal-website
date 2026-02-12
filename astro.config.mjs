6d67e53fdef96089bb84bb3896088d07f65131d0
hbleydal <bley.helene@hotmail.fr>
2026-02-12 16:18:21 +0100

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hbleydal.github.io',
+  site: 'https://hbleydal.github.io/personal-website',
   base: '/personal-website',
   output: 'static',
 
